816cfa161aa76451ed1c6b9c707f3f1945ce3531
JamieOnSootMC <jamie@sootmc.net>
2023-04-24 15:39:16 +0100

    Fix a couple bugs, need to test later

--- src/main/java/net/sootmc/chattoweb/Listeners/ConnectionListener.java
+++ src/main/java/net/sootmc/chattoweb/Listeners/ConnectionListener.java
@@ -1,5 +1,6 @@
 package net.sootmc.chattoweb.Listeners;
 
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -12,9 +13,22 @@ public class ConnectionListener implements Listener {
         String name = event.getPlayer().getName();
         String uuid = event.getPlayer().getUniqueId().toString();
         String serverConnected = event.getPlayer().getServer().getInfo().getName();
-        String serverDisconnected = event.getFrom().getName();
 
+        if(event.getFrom() == null) {
+            HttpHandler.makeConnectionRequest(name, uuid, serverConnected, "connect");
+            return;
+        }
+        String serverDisconnected = event.getFrom().getName();
         HttpHandler.makeConnectionRequest(name, uuid, serverConnected, "connect");
         HttpHandler.makeConnectionRequest(name, uuid, serverDisconnected, "disconnect");
+    }
+
+    @EventHandler
+    public void onQuit(PlayerDisconnectEvent event) {
+        String name = event.getPlayer().getName();
+        String uuid = event.getPlayer().getUniqueId().toString();
+        String server = event.getPlayer().getServer().getInfo().getName();
+
+        HttpHandler.makeConnectionRequest(name, uuid, server, "disconnect");
     }
 }
